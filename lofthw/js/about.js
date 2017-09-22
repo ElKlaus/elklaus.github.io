a39121b4fb092037c92b199e379f3613aed827fb
Sergey Kirov <klaus15rus@gmail.com>
2017-09-22 16:42:42 +0400

    CORRECT: hero section

--- lofthw/js/about.js
+++ lofthw/js/about.js
@@ -1,1 +1,1 @@
-webpackJsonp([3],{10:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var c=t(0),o=(t.n(c),t(11));t.n(o)},11:function(e,n){}},[10]);
+webpackJsonp([4],{10:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var c=t(0),o=(t.n(c),t(11));t.n(o)},11:function(e,n){}},[10]);
---
+++ lofthw/js/works.js
@@ -0,0 +1,1 @@
+webpackJsonp([2],{12:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var c=t(0),o=(t.n(c),t(13));t.n(o)},13:function(e,n){}},[12]);
